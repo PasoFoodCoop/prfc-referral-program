e2c6531cdd7f7d1d600fddea091660b7595afcb7
PasoFoodCoop <kermit.komm@yahoo.com>
2025-06-04 10:44:26 -0700

    Update ReferralForm.tsx

--- src/components/ReferralForm/ReferralForm.tsx
+++ src/components/ReferralForm/ReferralForm.tsx
@@ -96,8 +96,7 @@ export default function ReferralForm() {
         setYourEmail("");
         setShowConfirmation(true); // Show confirmation popup
       } else {
-        var postbody = JSON.stringify(referralData); //DEBUG
-        alert(postbody); //DEBUG
+        alert("response.OK failed with result from api/referral" ); //DEBUG
         setErrorMessage("Failed to submit the form. Please try again!");
       }
     } catch (error) {
