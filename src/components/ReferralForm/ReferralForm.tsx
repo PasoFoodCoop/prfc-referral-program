73f81c01aa15887490df499d22833904a975caee
PasoFoodCoop <kermit.komm@yahoo.com>
2025-07-29 13:57:56 -0700

    Update ReferralForm.tsx
    comment fix

--- src/components/ReferralForm/ReferralForm.tsx
+++ src/components/ReferralForm/ReferralForm.tsx
@@ -93,7 +93,7 @@ export default function ReferralForm() {
       if (response.ok) {
         console.log("Referral created successfully");
         setProspects([{ email: "", fullName: "" }]);
-        //setYourEmail(""); //do not reset
+        // setYourEmail(""); do not reset
         setShowConfirmation(true); // Show confirmation popup
       } else {
         alert("response OK failed with result from api/referral"); // DEBUG
